e15d69b3fa6999298b8230e12eb50725c5807080
Kraak, Louis CSNR:EX <Louis.Kraak@gov.bc.ca>
2019-06-13 16:13:52 -0700

    bluetooth scanning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "Home",
     moduleId: module.id,
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
 
@@ -12,6 +13,29 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        
         // Init your component properties here.
     }
+
+    scan() {
+        var bluetooth = require("nativescript-bluetooth");
+        bluetooth.isBluetoothEnabled().then(
+            function(enabled){
+                console.log("Enabled? " + enabled);
+                bluetooth.startScanning({
+                    serviceUUIDs: [],
+                    seconds: 4,
+                    onDiscovered: function(peripheral){
+                        console.log("Peripheral found with UUID: " + peripheral.UUID);
+                        console.log("Peripheral's name: " + peripheral.name);
+                    }
+                }).then(function() {
+                    console.log("scanning complete");
+                }, function(err) {
+                    console.log("error while scanning: " + err);
+                });
+            },
+        );
+    }
+
 }
