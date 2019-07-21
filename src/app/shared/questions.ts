f5cc67dcd0e641f441f631e48049757df45b001a
Krocodial <xxkraaklxx@gmail.com>
2019-07-21 12:27:39 -0700

    quiz cosmetic

--- src/app/shared/questions.ts
+++ src/app/shared/questions.ts
@@ -2,59 +2,95 @@ export var json =
 {
     "categories": [
         {
-            "title": "Configuration Quiz",
+            "title": "UV Tolerance Quiz",
             "quizDate": "20 October",
             "lastScore": "70%",
-            "backgroundColor": "#250046",
-            "image": "~/app/images/quiz.jpg",
+            "backgroundColor": "#51A3A3",
+            "image": "~/app/images/quiz.png",
             "questions": [
                 {
-                    "question": "Which of the following is correct about Angular 2 Components?",
+                    "question": "which color is closest to your skin color?",
                     "options": [
-                        "AngularJS had a focus of Controllers but Angular 2 has changed the focus to having components over controllers.",
-                        "Components help to build the applications into many modules.",
-                        "This helps in better maintaining the application over a period of time.",
-                        "All of the above."
+                        {
+                            "option": "",
+                            "color": "saddlebrown"
+                        },
+                        {
+                            "option": "",
+                            "color": "peru"
+                        },
+                        {
+                            "option": "",
+                            "color": "tan"
+                        },
+                        {
+                            "option": "",
+                            "color": "cornsilk"
+                        }
                     ],
                     "correctAnswerIndex": 3
                 },
                 {
-                    "question": "Which of the following is true?",
+                    "question": "Have any of your relatives been diagnosed with skin cancer?",
                     "options": [
-                        "Angular 2 Modules are used to create components which can be shared across the entire application.",
-                        "Angular 2 Metadata is used to create components which can be shared across the entire application.",
-                        "Angular 2 Services are used to create components which can be shared across the entire application.",
-                        "None of the above."
+                        {
+                            "option": "Yes"
+                        },
+                        {
+                            "option": "No"
+                        },
+                        {
+                            "option": "Unsure"
+                        }
                     ],
                     "correctAnswerIndex": 2
                 },
                 {
-                    "question": "Which of the following is correct about Import Array in Angular 2 Modules?",
+                    "question": "Are you currently taking medication with [xxxx], [yyyy], or [aaaa] in it?",
                     "options": [
-                        "Import array can be used to import the functionality from other Angular JS modules.",
-                        "Import array can be used to import the templates.",
-                        "Both of the above.",
-                        "None of the above."
+                        {
+                            "option": "Yes"
+                        },
+                        {
+                            "option": "No"
+                        }
                     ],
                     "correctAnswerIndex": 0
                 },
                 {
-                    "question": "Which of the following is correct about CLI?",
+                    "question": "If you have applied sunscreen today what was the SPF?",
                     "options": [
-                        "CLI stands for Command Line Interface.",
-                        "CLI can be used to create Angular JS application.",
-                        "It also helps in creating a unit and end-to-end tests for the application.",
-                        "All of the above."
+                        {
+                            "option": "SPF 15"
+                        },
+                        {
+                            "option": "SPF 30"
+                        },
+                        {
+                            "option": "SPF 60"
+                        },
+                        {
+                            "option": "I haven't applied sunscreen today"
+                        }
+
                     ],
                     "correctAnswerIndex": 3
                 },
                 {
-                    "question": "Which of the following is correct about systemjs.config.json?",
+                    "question": "How long ago?",
                     "options": [
-                        "This file is used to give the options about TypeScript used for the Angular JS project.",
-                        "This file contains information about Angular 2 project.",
-                        "This file contains the system files required for Angular JS application.",
-                        "All of the above."
+                        {
+                            "option": "< 30mins"
+                        },
+                        {
+                            "option": "30mins > 1 hour"
+                        },
+                        {
+                            "option": "1 > 2 hours"
+                        },
+                        {
+                            "option": "I haven't applied sunscreen today"
+                        }
                     ],
                     "correctAnswerIndex": 2
                 }
@@ -64,8 +100,47 @@ export var json =
             "title": "Statistics",
             "quizDate": "20 October",
             "lastScore": "70%",
-            "backgroundColor": "#0D0735",
+            "backgroundColor": "#B8CDF8",
             "image": "~/app/images/stats.png",
+            "questions": [
+                {
+                    "question": "How long is an IPv6 address?",
+                    "options": [
+                        "32 bits",
+                        "128 bytes",
+                        "64 bits",
+                        "128 bits"
+                    ],
+                    "correctAnswerIndex": 3
+                },
+                {
+                    "question": "What flavor of Network Address Translation can be used to have one IP address allow many users to connect to the global Internet?",
+                    "options": [
+                        "NAT",
+                        "Static",
+                        "Dynamic",
+                        "PAT"
+                    ],
+                    "correctAnswerIndex": 3
+                },
+                {
+                    "question": "Which of the following is the valid host range for the subnet on which the IP address 192.168.168.188 255.255.255.192 resides?",
+                    "options": [
+                        "192.168.168.129-190",
+                        "192.168.168.129-191",
+                        "192.168.168.128-190",
+                        "192.168.168.128-192"
+                    ],
+                    "correctAnswerIndex": 0
+                }
+            ]
+        },
+        {
+            "title": "Device Info",
+            "quizDate": "",
+            "lastScore": "",
+            "backgroundColor": "#DD7373",
+            "image": "~/app/images/device_info.png",
             "questions": [
                 {
                     "question": "How long is an IPv6 address?",
